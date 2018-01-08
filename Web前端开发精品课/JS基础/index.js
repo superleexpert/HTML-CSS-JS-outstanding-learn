8f5848cf777e6579cc9520536c07d07a116d38d5
superleexpert <superleexpert@gmail.com>
2018-01-08 13:25:13 +0800

    Initlialize step

---
+++ Web前端开发精品课/JS基础/index.js
@@ -0,0 +1,19 @@
+document.write("我的JS学习")
+function alertMessage() {
+    alert('跑起来了');
+}
+var a = Number("2018") + 1000;
+document.write(a + "<br/>");
+
+var b = parseInt("123");
+document.write(b +"<br/>");
+
+var x, y;
+x=10;
+y=x++;
+document.write("x"+x+"  "+"y"+y);
+
+function linkFuncTest() {
+    // document.write("超链接调用函数成功");
+    alert("超链接调用函数成功");
+}
